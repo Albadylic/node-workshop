35c99bf84bb1b01567f04f874eebb4be91999f3b
Gregor <gregorlthomson@sky.com>
2019-07-22 15:11:55 +0100

    Deliver HTML

--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
 const http = require("http");
-
-const server = http.createServer(handler);
-
-const message = "Hi, from G and G";
+const fs = require("fs");
 
 function handler(request, response) {
-  response.writeHead(200, { "content-type": "text/html" });
-  response.write(message);
-  response.end();
+  const endpoint = request.url;
+  const method = request.method;
+
+  if (endpoint === "/") {
+    response.writeHead(200, { "content-type": "text/html" });
+
+    fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      response.end(file);
+    });
+  }
 }
+
+const server = http.createServer(handler);
 
 server.listen(3000, function() {
   console.log("Server listening on port 3000, ready to accept requests ;)");
