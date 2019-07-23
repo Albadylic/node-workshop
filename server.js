91f5f7e0586b4f77f05916867f544d53dfc34c9a
Gregor <gregorlthomson@sky.com>
2019-07-23 12:59:00 +0100

    Nuke Code

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const handler = require("./src/handler");
-
-const server = http.createServer(handler);
-
-server.listen(3000, function() {
-  console.log("Server listening on port 3000, ready to accept requests ;)");
-});
